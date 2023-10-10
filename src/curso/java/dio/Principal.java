51d31b51136b3fff736b9ec5ce0eca8e3a307fcf
crist <crist@DESKTOP-JSSE25O>
2023-10-09 21:24:52 -0300

    Primeiro commint realizado com um projeto já pronto

---
+++ src/curso/java/dio/Principal.java
@@ -0,0 +1,43 @@
+
+package curso.java.dio;
+
+//as classes devem ser nomeadas sempre com as primeiras leras em maiuscula
+//inclusive as compostas ex: CalculadoraCientifica
+
+public class Principal {
+
+   //Variáveis com tipo "final" estão protegidas de modificações. ex:
+    //String BR = "Brasil"; (Está variavel poderá ser modificada a qualquer momento dentro da aplicação)
+    //final String BR = "Brasil"; (A palavra "final" no inicio da variavel, protega ela de alterações ao longo do programa
+    
+    /* VARIAVEIS:
+    Deve conter apenas letras, _ (underline), $ ou os números de 0 a 9;
+    Deve obrigatoriamente se iniciar por uma letra (preferencialmente), _ ou $, jamais com número;
+    Deve iniciar com uma letra minúscula (boa prática – ver abaixo);
+    Não podemos usar palavras-chave da linguagem;
+    O nome deve ser único dentro de um escopo.
+    */
+    
+    /*
+    Declarar uma variável em Java segue sempre a seguinte estrutura:
+    Tipo NomeBemDefinido = Atribuicao (opcional em alguns casos)
+    Dog spike; //observe que aqui a variável spike não tem valor, é normal
+    Declarar métodos em Java segue uma estrutura bem simples:
+    TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+    int somar (int numeroUm, int numero2)
+    String formatarCep (long cep)
+    
+    */
+    public static void main(String[] args) {
+        
+        /*
+        
+        
+        */
+        
+        System.out.println(" \nOlá Mundo!\n");
+        
+      
+    }
+    
+}
